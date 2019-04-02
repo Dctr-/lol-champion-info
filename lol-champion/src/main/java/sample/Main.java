e1e155e19eb52cb03b0ae290e6787f01e079f557
Dctr <hiimdctr@gmail.com>
2019-04-02 04:35:57 -0400

    cleanup

--- lol-champion/src/main/java/sample/Main.java
+++ lol-champion/src/main/java/sample/Main.java
@@ -22,7 +22,6 @@ public class Main extends Application {
         return Main.primaryStage;
     }
 
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         setPrimaryStage(primaryStage);
